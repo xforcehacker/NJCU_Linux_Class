8aa502cbdb03f4685ae14d9aa71d631ff293bff3
Mackensie Alvarez <greymattermack@gmail.com>
2019-02-21 10:17:40 -0500

    home work 4 changes (#9)

---
+++ Week4/MackensieAlvarez/prog1.sh
@@ -0,0 +1,7 @@
+count=0
+while [ $count -le 10 s]
+do
+	echo "number " + $count
+    count=$(( $count + 1 ))
+done
+ 
---
+++ Week4/MackensieAlvarez/prog3.sh
@@ -0,0 +1,7 @@
+number = 5
+if test $number -le 18
+then
+	echo "Number equal to 18"
+else
+	echo "Number is not to 18"
+fi
