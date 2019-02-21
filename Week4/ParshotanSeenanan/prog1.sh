b9ef4597ed535f70f3336254386961dbe7eeac8c
Parshotan Seenanan <parshsee@gmail.com>
2019-02-21 00:18:00 -0500

    Homework Assignment 4 by Parsh

---
+++ Week4/ParshotanSeenanan/prog1.sh
@@ -0,0 +1,9 @@
+#While Loop
+
+i=0
+
+while [ $i -le 10 ]
+do
+	echo $i
+	i=$[$i + 1]
+done
---
+++ Week4/ParshotanSeenanan/prog2.sh
@@ -0,0 +1,9 @@
+#For Loop
+
+arr=("apple" "banana" "whateverfruithasC" "Danimals" "eggo" "frenchfries")
+
+for i in ${arr[@]}
+do
+	echo $i
+done
+
