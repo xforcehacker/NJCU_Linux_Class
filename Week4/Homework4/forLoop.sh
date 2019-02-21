ce08ba321a38559ffaba1d5b7fe347b974d401ee
Richard Diaz <rdiaz6@njcu.edu>
2019-02-21 23:42:05 +0000

    i did all the requirements for hw 4 week 4

---
+++ Week4/Homework4/forLoop.sh
@@ -0,0 +1,10 @@
+#!bin/bash
+
+for a in 1 2 3
+do
+	for b in helloRich holaRich
+	do
+		echo "$a $b"
+	done
+done
+# i did a nested loop playing around with for loops to print out 1 hello rich 2 hello rich ..... and so on also this runs using bash command not ./
