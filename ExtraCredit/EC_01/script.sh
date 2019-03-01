78bbb08d46f9b2944c7cd194de659dd73a3540d5
melvyniandrag <melvyndrag@yahoo.com>
2019-03-01 13:50:11 -0500

    Update script.sh

--- ExtraCredit/EC_01/script.sh
+++ ExtraCredit/EC_01/script.sh
@@ -1,7 +1,15 @@
+# For an example of how this script works, replace file.txt with sample_file.txt.
+# then replace patterns.txt with sample_patterns.txt
+# You will see the expected output.
+
+INPUT_FILE="file.txt"
+TMP_FILE="output.txt"
+PAT_FILE="patterns.txt"
+
 while read line
 do
         noNewLine=$( echo $line | tr -d "\n")
-        grep -v "$noNewLine" file.txt > output.txt
-        mv output.txt file.txt
-        wc -l file.txt
-done < patterns.txt
+        grep -v "$noNewLine" $INPUT_FILE > $TMP_FILE
+        mv $TMP_FILE $INPUT_FILE
+        wc -l $INPUT_FILE
+done < $PAT_FILE
