8299bff7c7c9a21a0c5050e270d8bb8b00c736a2
melvyniandrag <melvyndrag@yahoo.com>
2019-03-01 13:48:01 -0500

    Update script.sh

--- ExtraCredit/EC_01/script.sh
+++ ExtraCredit/EC_01/script.sh
@@ -1,8 +1,7 @@
 while read line
 do
         noNewLine=$( echo $line | tr -d "\n")
-        echo "grep -v \"$noNewLine\" file.txt > output.txt "
         grep -v "$noNewLine" file.txt > output.txt
         mv output.txt file.txt
         wc -l file.txt
-done < patterns
+done < patterns.txt
