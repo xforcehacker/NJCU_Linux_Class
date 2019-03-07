959b524280f236f732797cfd170da08ce8fb23ac
retro <punisher41@gmail.com>
2019-03-07 01:49:57 -0500

    worked in hw 6

--- Week6/FlaskSite/runCurl.sh
+++
@@ -1,8 +0,0 @@
- #curl -X POST -F 'inputOne=hello' \
- #        -F 'inputTwo=world' \
- #        localhost:80
-
-
- curl -X PUT -H 'Content-Type: application/json' \
-        -d '{"name": "Melvyn","age": 30}' \
-        localhost/professor
