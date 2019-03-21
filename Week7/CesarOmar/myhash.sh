cfdd692d34a0dba1cdbfabae9e66dda153f50411
Cesar Omar Sanchez <30625358+cesaromar1973@users.noreply.github.com>
2019-03-21 18:55:31 -0400

    Add files via upload

--- Week7/CesarOmar/myhash.sh
+++ Week7/CesarOmar/myhash.sh
@@ -1,8 +1,7 @@
-!#/bin/bash
+#!/bin/bash
 # myhash.sh
 # Cesar Sanchez
 
-s-a-number-in-bash
 inputVal=$1
 echo "myhash being run with $inputVal"
 STRING_HASH_VAL=0
@@ -12,13 +11,14 @@ NUMBER_RE='^[-]?[0-9]+$'
 EVEN_RE=__TODO__REGEX_FOR_EVEN_NUMBER
 if [[ $inputVal =~ $NUMBER_RE ]] 
 then
-        if [[ "$string" =~ ^-?[0-9]+[.,]?[0-9]*$ ]]
-then
-    echo $string is a number
-else
-    echo $string is not a number
-fi
-else # must be a string
-        echo $-12,345;
-fi
-exit 0
+	        if [[ "$string" =~ ^-?[0-9]+[.,]?[0-9]*$ ]]
+		then
+			    echo $string is a number
+		    else
+			        echo $string is not a number
+			fi
+		else # must be a string
+			        echo $-12,345;
+			fi
+			exit 0
+			
