93d5cd877a67ea216930cd87112ad4ef8b66d181
melvyniandrag <melvyndrag@yahoo.com>
2019-04-05 14:59:46 -0400

    Create pass_params.sh

---
+++ Week10/pass_params.sh
@@ -0,0 +1,10 @@
+#!/bin/bash
+#
+# Run this script like:
+# ./myscript 1 2 3 4 5 6 7 8 9 10 a b c
+# 
+# The purpose of this script is to show you how to pass positional parameters
+# from the command line to a bash script.
+#
+
+echo $1 $2 $3 $4 $5 $6 $7 $8 $9 ${10} ${11} ${12} ${13}
