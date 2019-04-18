75594b3aaa841682bb3745ae38bf6d7c5a4ec786
Steven Surujballi <stevesurujballi@gmail.com>
2019-04-18 18:50:25 -0400

    Update awk_and_sed.sh

--- Week11/StevenSurujballi/Version 2/awk_and_sed.sh
+++ Week11/StevenSurujballi/Version 2/awk_and_sed.sh
@@ -4,7 +4,7 @@
 awk '/\320\244\321\221\320\264\320\276\321\200/ { print "Fyodor" }' sample_text.txt
 awk '/\x72\x65\x63\x6f\x72\x64\x61\xc3\xa7\xc3\xa3\x6f/ { print "memory" }' sample_text.txt
 awk '/\156\151\303\261\157/ { print "boy" }' sample_text.txt
-awk '/\xF0\x9F\x98\x82/ { print "found ROFL!" }' sample_text.txt
+awk '/\xf0\x9f\x98\x82/ { print "found ROFL!" }' sample_text.txt
 
 echo -e "\n"
 # 4 sed statements
