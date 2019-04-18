88e6ba678b4f475624ede0e23afc06326f36c078
ElizabethRappleyea <elizabeth.rappleyea7@gmail.com>
2019-04-18 21:09:19 +0000

    sed im pretty awk, heres hw11

--- Week11/ElizabethRappleyea/awk_and_sed.sh
+++ Week11/ElizabethRappleyea/awk_and_sed.sh
@@ -18,4 +18,4 @@ echo -e "sed:\n"
 sed -n 's/\o320\o244\o321\o221\o320\o264\o320\o276\o321\o200/Fyodor/p' sample_text.txt
 sed -n 's/\x72\x65\x63\x6f\x72\x64\x61\xc3\xa7\xc3\xa3\x6f/memory/p' sample_text.txt
 sed -n 's/\o156\o151\o303\o261\o157/boy/p' sample_text.txt
-sed -n 's/\xf0\x9f\x98\x81/found ROFL/p' sample_text.txt
+sed -n 's/^\xf0\x9f\x98\x81\{3,5\}$/found ROFL/p' sample_text.txt
